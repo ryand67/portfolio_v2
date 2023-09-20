c64ad492a6739a9b9a61c173f965bb87f202e797
Ryan Devenney <rwdevenney@gmail.com>
2023-09-20 13:59:07 -0400

    favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,8 @@ import resumeLogo from './assets/resume.png';
 export default function Home() {
   return (
     <main className="bg-black flex justify-center min-h-screen h-screen flex-col items-center p-24">
-        <h1 className="text-8xl text-center font-mono">Ryan Devenney</h1>
+        <h1 className="text-8xl text-center font-mono">Ryan</h1>
+        <h1 className="text-8xl text-center font-mono">Devenney</h1>
         <h4 className="text-xl font-mono">Backend Engineer | Vim Enthusiast</h4>
         <div className='flex flex-row justify-center items-center'>
             <SocialLogo href="https://www.github.com/ryand67" 
