bac3a04b1fe99e256cfb3cc016cbf3953a503f6d
Ryan Devenney <rwdevenney@gmail.com>
2023-09-20 13:49:49 -0400

    desc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Ryan Devenney',
+  title: 'Ryan Devenney | Backend Software Engineer',
   description: 'Backend Software Engineer | Vim Enthusiast',
 }
 
